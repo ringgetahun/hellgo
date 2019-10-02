661cf1015834fa2b1b1fe54ee2d5771b62e5dbac
ringgetahun <ring_getahun@yahoo.com>
2019-10-02 12:56:37 -0500

    Added common line

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+func translate(input string) string {
+	var transilations = make(map[string]string)
+	transilations["en"] = "Hello"
+	transilations["de"] = "Guten Tag"
+	transilations["fr"] = "Bonjure"
+	return transilations[input]
+}
 func main() {
-	var locale, greeting string
-	var languages = [4]string{"en", "es", "de", "fr"}
+	var locale string
+	if len(os.Args) == 1 {
+		fmt.Println("Enter language code:")
+		fmt.Scanf("%s", &locale)
+	} else {
+		locale = os.Args[1]
+	}
+	output := translate(locale)
+	if output == " " {
+		output = "Yo"
+	}
+	/*var transilations=make(map[string]string)
+	transilations["en"]="Hello"
+	transilations["de"]="Guten Tag"
+	transilations["fr"]="Bonjure"
+	return translations[input] */
+
+	/*var languages = [4]string{"en", "es", "de", "fr"}
 	locale = languages[1]
 
+
+	fmt.Scanf("Enter language code:")
+	fmt.Scanf("%s", &locale)
 	/*
 		if locale == "en" {
 			greeting = "Hello"
@@ -17,7 +46,7 @@ func main() {
 		} else {
 			greeting = "Yo"
 		}
-	*/
+
 
 	switch locale {
 	case "en":
@@ -33,4 +62,6 @@ func main() {
 	}
 
 	fmt.Printf(greeting + ", Go!\n")
+	*/
+	fmt.Println(output, "Go!")
 }
